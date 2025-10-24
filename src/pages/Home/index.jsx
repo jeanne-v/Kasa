e05036397b0bba64d7eea753039b04b38b422092
Jeanne Viallard <viallard.jeanne@gmail.com>
2025-10-24 10:27:51 +0200

    Refactor data fetch + redirect on property id error

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import logements from "../../data/logements.json";
+import { useState, useEffect } from "react";
 
 import Card from "../../components/Card";
 import Banner from "../../components/Banner";
@@ -7,6 +7,27 @@ import "./Home.scss";
 import bannerBg from "../../assets/banner-bg-home.jpg";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchLogements() {
+      try {
+        const res = await fetch("/logements.json");
+        const logementsData = await res.json();
+        setData(logementsData);
+      } catch (err) {
+        setError(err);
+      }
+    }
+
+    fetchLogements();
+  }, []);
+
+  if (error) {
+    return <p>une erreur est survenue</p>;
+  }
+
   return (
     <div className="home">
       <Banner bg={bannerBg}>
@@ -14,18 +35,20 @@ export default function Home() {
           <span>Chez vous, </span>partout et ailleurs
         </h1>
       </Banner>
-      <div className="home__cards">
-        {logements.map((logement) => {
-          return (
-            <Card
-              key={logement.id}
-              title={logement.title}
-              cover={logement.cover}
-              id={logement.id}
-            />
-          );
-        })}
-      </div>
+      {data && (
+        <div className="home__cards">
+          {data.map((logement) => {
+            return (
+              <Card
+                key={logement.id}
+                title={logement.title}
+                cover={logement.cover}
+                id={logement.id}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/Property/index.jsx
+++ src/pages/Property/index.jsx
@@ -1,4 +1,5 @@
-import { useParams } from "react-router";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
 
 import SlideShow from "../../components/SlideShow";
 import Tag from "../../components/Tag";
@@ -6,47 +7,75 @@ import Rating from "../../components/Rating";
 import HostInfos from "../../components/HostInfos";
 import Collapse from "../../components/Collapse";
 
-import propertiesData from "../../data/logements.json";
-
 import "./Property.scss";
 
 export default function Property() {
   const params = useParams();
-  const data = propertiesData.find((property) => property.id === params.id);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchLogement() {
+      try {
+        const res = await fetch("/logements.json");
+        const logementsData = await res.json();
+        const logementData = logementsData.find((property) => property.id === params.id);
+        if (!logementData) {
+          navigate("/404");
+        } else {
+          setData(logementData);
+        }
+      } catch (err) {
+        setError(err);
+      }
+    }
+
+    fetchLogement();
+  }, [params, navigate]);
+
+  if (error) {
+    return <p>Une erreur est survenue</p>;
+  }
+
   return (
-    <div className="property">
-      <SlideShow pictures={data.pictures} />
+    <>
+      {data && (
+        <div className="property">
+          <SlideShow pictures={data.pictures} />
 
-      <div className="property__basic-infos">
-        <div>
-          <h1 className="property__title">{data.title}</h1>
-          <p className="property__location">{data.location}</p>
+          <div className="property__basic-infos">
+            <div>
+              <h1 className="property__title">{data.title}</h1>
+              <p className="property__location">{data.location}</p>
 
-          <div className="property__tags">
-            {data.tags.map((tag, index) => (
-              <Tag key={index} text={tag} />
-            ))}
+              <div className="property__tags">
+                {data.tags.map((tag, index) => (
+                  <Tag key={index} text={tag} />
+                ))}
+              </div>
+            </div>
+
+            <div className="property__rating-and-host">
+              <Rating rating={data.rating} />
+              <HostInfos name={data.host.name} picture={data.host.picture} />
+            </div>
+          </div>
+
+          <div className="property__details">
+            <Collapse title="Description" titleType="p">
+              <p>{data.description}</p>
+            </Collapse>
+            <Collapse title="Équipements" titleType="p">
+              <ul style={{ padding: 0, listStyleType: "none" }}>
+                {data.equipments.map((item, index) => {
+                  return <li key={index}>{item}</li>;
+                })}
+              </ul>
+            </Collapse>
           </div>
         </div>
-
-        <div className="property__rating-and-host">
-          <Rating rating={data.rating} />
-          <HostInfos name={data.host.name} picture={data.host.picture} />
-        </div>
-      </div>
-
-      <div className="property__details">
-        <Collapse title="Description" titleType="p">
-          <p>{data.description}</p>
-        </Collapse>
-        <Collapse title="Équipements" titleType="p">
-          <ul style={{ padding: 0, listStyleType: "none" }}>
-            {data.equipments.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </Collapse>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
